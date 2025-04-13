fc61a43d51010a89bc3a39d9135607780c6e5c1e
Hunter Koppen <hunter.koppen@kobeon.nl>
2025-04-13 17:52:33 +0200

    first logic for loading the image

--- src/ImageLabeling.jsx
+++ src/ImageLabeling.jsx
@@ -3,6 +3,14 @@ import { createElement } from "react";
 import { ImageKonva } from "./components/ImageKonva";
 import "./ui/ImageLabeling.css";
 
-export function ImageLabeling({ sampleText }) {
-    return <ImageKonva sampleText={sampleText} />;
+export function ImageLabeling({ image, labelList, labelTitle, labelColor, onChange }) {
+    return (
+        <ImageKonva 
+            image={image} 
+            labelList={labelList}
+            labelTitle={labelTitle}
+            labelColor={labelColor}
+            onChange={onChange} 
+        />
+    );
 }
